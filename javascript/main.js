75434c510238c32815529d20c51a0d239184fc1a
Mahmoud Ashraf Farouk <mashraffarouk2702@gmail.com>
2022-08-23 16:09:02 +0200

    add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,17 @@
+// store in local storage
+localStorage.setItem("darkMode", "hsl(235, 21%, 11%)");
+localStorage.setItem("lightMode", "hsl(0, 0%, 98%)");
+localStorage.setItem("darkBackground", "images/bg-desktop-dark.jpg");
+localStorage.setItem("lightBackground", "images/bg-desktop-light.jpg");
+localStorage.setItem("darkTodo", "hsl(235, 24%, 19%)");
+localStorage.setItem("lightTodo", "hsl(236, 33%, 92%)");
+localStorage.setItem("darkFont", "hsl(234, 39%, 85%)");
+localStorage.setItem("lightFont", "hsl(233, 14%, 35%)");
+localStorage.setItem("darkBorder", "1px solid hsl(237, 14%, 26%)");
+localStorage.setItem("lightBorder", "1px solid hsl(233, 11%, 84%)");
+localStorage.setItem("darkIcon", "images/icon-moon.svg");
+localStorage.setItem("lightIcon", "images/icon-sun.svg");
+
 let inputCheck = document.querySelector(".input .input-check");
 inputCheck.addEventListener("click", function () {
   this.classList.toggle("checked");
@@ -31,6 +45,18 @@ function addTodo() {
 </div>`;
     document.querySelector(".list-body").innerHTML += item;
     todo.value = "";
+    let todoItems = document.querySelectorAll(".item");
+    if (localStorage.getItem("currentMode") == "darkMode") {
+      todoItems.forEach(function (item) {
+        item.style.borderBottom = localStorage.getItem("darkBorder");
+        item.style.color = localStorage.getItem("darkFont");
+      });
+    } else {
+      todoItems.forEach(function (item) {
+        item.style.borderBottom = localStorage.getItem("lightBorder");
+        item.style.color = localStorage.getItem("lightFont");
+      });
+    }
 
     crossEvent();
     checkLength();
@@ -112,4 +138,85 @@ filters.forEach(function (filter) {
       });
     }
   });
+});
+
+let background = document.getElementById("myBackground");
+let todoList = document.querySelector(".list").children;
+let iconSwitch = document.querySelector(".icon-switch img");
+let inputItem = document.querySelector(".input input");
+
+if (localStorage.getItem("currentMode") == "darkMode") {
+  let todoItems = document.querySelectorAll(".item");
+
+  document.body.style.backgroundColor = localStorage.getItem("darkMode");
+  background.src = localStorage.getItem("darkBackground");
+  iconSwitch.src = localStorage.getItem("darkIcon");
+  inputItem.style.color = localStorage.getItem("darkFont");
+
+  for (item of todoList) {
+    item.style.backgroundColor = localStorage.getItem("darkTodo");
+  }
+  todoItems.forEach(function (item) {
+    item.style.borderBottom = "1px solid hsl(237, 14%, 26%)";
+    item.style.color = localStorage.getItem("darkFont");
+  });
+} else {
+  let todoItems = document.querySelectorAll(".item");
+
+  document.body.style.backgroundColor = localStorage.getItem("lightMode");
+  background.src = localStorage.getItem("lightBackground");
+  iconSwitch.src = localStorage.getItem("lightIcon");
+  inputItem.style.color = localStorage.getItem("darkFont");
+
+  for (item of todoList) {
+    item.style.backgroundColor = localStorage.getItem("lightTodo");
+  }
+  todoItems.forEach(function (item) {
+    item.style.borderBottom = "1px solid hsl(233, 11%, 84%)";
+    item.style.color = localStorage.getItem("lightFont");
+  });
+}
+
+// Switch Dark/Light Mode
+let switchMode = document.querySelector(".icon-switch");
+switchMode.addEventListener("click", function () {
+  let todoItems = document.querySelectorAll(".item");
+
+  if (localStorage.getItem("currentMode") == "darkMode") {
+    localStorage.setItem("currentMode", "lightMode");
+    document.body.style.backgroundColor = localStorage.getItem("lightMode");
+
+    // Change the background image through js
+    background.src = localStorage.getItem("lightBackground");
+    inputItem.style.color = localStorage.getItem("lightFont");
+
+    for (item of todoList) {
+      item.style.backgroundColor = localStorage.getItem("lightTodo");
+      item.style.color = localStorage.getItem("lightFont");
+    }
+    todoItems.forEach(function (item) {
+      item.style.borderBottom = "1px solid hsl(233, 11%, 84%)";
+      item.style.color = localStorage.getItem("lightFont");
+    });
+
+    iconSwitch.src = localStorage.getItem("lightIcon");
+  } else {
+    localStorage.setItem("currentMode", "darkMode");
+    document.body.style.backgroundColor = localStorage.getItem("darkMode");
+
+    // Change the background image through js
+    background.src = localStorage.getItem("darkBackground");
+    inputItem.style.color = localStorage.getItem("darkFont");
+
+    for (item of todoList) {
+      item.style.backgroundColor = localStorage.getItem("darkTodo");
+      item.style.color = localStorage.getItem("darkFont");
+    }
+    todoItems.forEach(function (item) {
+      item.style.borderBottom = "1px solid hsl(237, 14%, 26%)";
+      item.style.color = localStorage.getItem("darkFont");
+    });
+
+    iconSwitch.src = localStorage.getItem("darkIcon");
+  }
 });
